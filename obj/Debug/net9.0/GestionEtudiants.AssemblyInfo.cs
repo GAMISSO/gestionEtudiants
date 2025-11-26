d8c5db0ec3958a21ec18e52f6fe52d84518ed26c
GAMISSO <koffi-emmanuel.gamisso@ism.edu.sn>
2025-11-26 11:32:28 +0100

    nouveau

--- obj/Debug/net9.0/GestionEtudiants.AssemblyInfo.cs
+++ obj/Debug/net9.0/GestionEtudiants.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionEtudiants")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945ab3591761da6e01029444325ebdcfd84afe88")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionEtudiants")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionEtudiants")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
